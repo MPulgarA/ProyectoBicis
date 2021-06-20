5db80060c5f920a1f5f526ccac131017bdad1981
MPulgarA <matiaspulgaralarcon@gmail.com>
2021-06-19 22:16:07 -0400

    Diseño de página completa

---
+++ public/javascripts/map.js
@@ -0,0 +1,12 @@
+var mymap = L.map('mapid').setView([-37.4667, -72.35], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibXB1bGdhcmEiLCJhIjoiY2txNGpuOTRnMHU5dTJ2bXZrZG1uaG5jeSJ9.G9jx-xyseeSYjpYUxfGTig'
+}).addTo(mymap);
+
+var marker = L.marker([-37.4640, -72.35]).addTo(mymap);
